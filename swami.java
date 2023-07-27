25d14d119c897c638d05282a9e54dc820e9ed286
Sunil Rai <sunilrai7440@gmail.com>
2023-07-27 14:01:01 +0530

    this is sunil branch

---
+++ swami.java
@@ -0,0 +1,15 @@
+class swami
+
+{
+	int a=50;
+	String name="SVCE INDORE";
+}
+class vivekanand
+{
+public static void main(String[] args)
+{
+swami s1=new swami();
+System.out.println(s1.a);
+System.out.println(s1.name);
+}
+}
